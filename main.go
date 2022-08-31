dee6edba42259b69b12986cc8334d917423cdc13
Philip Lewis <philipjonlewis@gmail.com>
2022-09-01 00:38:18 +0800

    initial crud server setup

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -17,6 +18,28 @@ var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book,err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"Book Not Found"})
+		return 
+	}
+
+	c.IndentedJSON(http.StatusOK,book)
+}
+
+func getBookById(id string) (*book,error) {
+	for i,b := range books {
+		if b.ID == id {
+			return &books[i],nil
+		}
+	}
+
+	return nil,errors.New("book not Found")
 }
 
 func getBooks(c *gin.Context) {
@@ -37,14 +60,66 @@ func createBook(c *gin.Context) {
 
 }
 
+func checkoutBook(c *gin.Context) {
+	id,ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Missing Id Query parameter"})
+	}
+
+	book ,err := getBookById(id)
+	
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"Book Not Found"})
+		return 
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Book Not Available"})
+		return 
+	}
+
+	book.Quantity -= 1
+
+	c.IndentedJSON(http.StatusOK,book)
+
+}
+
+func returnBook(c *gin.Context) {
+	id,ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Missing Id Query parameter"})
+	}
+
+	book ,err := getBookById(id)
+	
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"Book Not Found"})
+		return 
+	}
+
+	
+	book.Quantity += 1
+
+	c.IndentedJSON(http.StatusOK,book)
+
+}
+
 func main () {
 
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
 
+	router.GET("/books/:id", bookById)
+
+	router.PATCH("/checkout",checkoutBook)
+
+	router.PATCH("/return",returnBook)
+
 	router.POST("/books", createBook)
-	
+
 	router.Run("localhost:8080")
 
 
